bf91ee7e092b235090e0abd3ba4107f58d1f7e44
doctafaustus <clowd150@gmail.com>
2018-10-17 22:19:11 -0500

    Reconfigure server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.get('/', (req, res) => {
+  console.log('/ requestioned');
+  console.log(`${__dirname}/dist/index.html`);
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000!');
+});
