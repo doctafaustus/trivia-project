09b0de3fa8a57acca3350ef8d05d4c4c8ca7e574
doctafaustus <clowd150@gmail.com>
2018-12-05 23:58:39 -0600

    Hide avatar upload overlay after showing image preview

--- src/js/profile-update.js
+++ src/js/profile-update.js
@@ -7,22 +7,31 @@ export default function profileUpdate() {
 
   // Show preview of uploaded file
   $('#profile-image-upload-input').change(function() {
-    console.log('I was changed!');
     const reader = new FileReader();
     reader.addEventListener('load', e => {
       $('#profile-image').attr('src', e.target.result);
     });
     reader.readAsDataURL(this.files[0]);
+
+    // Remove initial overlay
+    $('.player-icon').removeClass('init-edit');
   });
 
   // Save information to database
   $('.profile-save').click(function() {
     const enteredName = $('#player-name-input').val();
-    if (enteredName.length > 22) return console.log('too long');
-    if (enteredName.length < 5) return console.log('too short');
-    console.log(enteredName);
+    const validation = validateUsername(enteredName);
+    if (validation) {
+      console.log('show');
+    };
   });
 
-  // TODO: Style upload image preview to sizing that will eventually be shown
+  // TODO: Show username validation messages in DOM
 }
 
+function validateUsername(username) {
+  let errorMessage = '';
+  if (username.length > 22) errorMessage = 'Username too long (max 22 characters)';
+  if (username.length < 5) errorMessage = 'Username too short (min 5 characters)';
+  return errorMessage;
+}
